aef842f7d0dfb1308b2f8d688a1b59469c32a6be
Gayathrivadlamudi <gayathrichowdary441@gmail.com>
2025-12-26 11:28:33 +0530

    creating new feautre in branch1

---
+++ scikit/branching/b1.py
@@ -0,0 +1,8 @@
+import numpy as np
+randNum=np.random.randint(1,10)
+print(randNum)
+randNum=np.random.randint(1,10,5)
+print(randNum)
+randNum=np.random.rand(5)
+print(randNum)
+print("Creating new branch and checking")
